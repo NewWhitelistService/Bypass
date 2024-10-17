0b55fa80d22d5756fc11fcc3a9fd482d84a8846b
NewWhitelistService <doitenroiugphone@gmail.com>
2024-10-17 19:27:29 +0700

    Update hwid.js

--- api/hwid.js
+++ api/hwid.js
@@ -1,9 +1,9 @@
 export default function handler(req, res) {
     if (req.method === 'POST') {
         const { hwid } = req.body; // Get the HWID from the request body
-
-        // Respond with the received HWID or any response data
-        res.status(200).json({ received: hwid });
+        
+        // Respond with just the HWID as a plain string
+        res.status(200).send(hwid);
     } else {
         res.setHeader('Allow', ['POST']);
         res.status(405).end(`Method ${req.method} Not Allowed`);
